f47c6037a0008beb34b17ad66dc6bd0dace36a54
Girish Ghugare <girishghugare19@gmail.com>
2024-08-09 05:43:06 +0530

    components screens and react router dom added

---
+++ instafresh/src/screens/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navabar from '../components/Navabar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <div>
+            <Navabar/>
+        </div>
+        <div>
+            Body
+        </div>
+        <div>
+            <Footer/>
+        </div>
+    </div>
+  )
+}
