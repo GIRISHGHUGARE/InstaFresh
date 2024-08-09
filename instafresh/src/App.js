5ca58cca6c77e4ec38d8b4a999ced663e8e474f1
Girish Ghugare <girishghugare19@gmail.com>
2024-08-09 06:08:35 +0530

    routers route path link added

--- instafresh/src/App.js
+++ instafresh/src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import Home from './screens/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+import Login from './screens/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <>
-    <div>
-      <Home/>
-    </div>
-    </>
+    <RouterProvider router={router} />
   );
 }
 
